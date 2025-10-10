af577c038c8515a7a97a4520fc95fe2e2e8362b0
ThomasBfrd <thomas.bfrd@gmail.com>
2025-10-10 14:58:17 +0200

    feat: configuration du projet

---
+++ src/assets/configuration/configuration.ts
@@ -0,0 +1,6 @@
+export const VITE_CLIENT_ID = import.meta.env.SPOTIFY_CLIENT_ID;
+export const VITE_REDIRECT_URI = import.meta.env.REDIRECT_URI ?? "";
+export const VITE_AUTH_ENDPOINT = import.meta.env.AUTH_ENDPOINT
+export const VITE_RESPONSE_TYPE = import.meta.env.RESPONSE_TYPE
+export const VITE_SCOPES = import.meta.env.SCOPES ?? ""
+export const url: string = `${VITE_AUTH_ENDPOINT}?client_id=${VITE_CLIENT_ID}&redirect_uri=${encodeURIComponent(VITE_REDIRECT_URI)}&response_type=${VITE_RESPONSE_TYPE}&scope=${encodeURIComponent(VITE_SCOPES)}&show_dialog=true`;
