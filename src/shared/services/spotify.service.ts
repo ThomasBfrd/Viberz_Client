fcbd0732b91a5df450e0743c1ddb66c6f71007ed
ThomasBfrd <thomas.bfrd@gmail.com>
2025-11-13 16:20:57 +0100

    feat: corrections pour les jeux guess

---
+++ src/shared/services/spotify.service.ts
@@ -0,0 +1,15 @@
+export const spotifyService = {
+    playSong: async (deviceId: string, trackId: string) => {
+        return fetch(`https://api.spotify.com/v1/me/player/play?device_id=${deviceId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                uris: [`spotify:track:${trackId}`],
+                position_ms: 0
+            }),
+        });
+    }
+
+}
