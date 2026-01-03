7db2cba2f5bb604951bf3fb2c6b691b51e415bf3
ThomasBfrd <thomas.bfrd@gmail.com>
2026-01-03 18:34:07 +0100

    feat: ajout d'un variable d'environnement pour le client url

--- src/shared/services/authentication.service.ts
+++ src/shared/services/authentication.service.ts
@@ -1,9 +1,10 @@
 export function initiateSpotifyAuth() {
-        const params = new URLSearchParams({
+    const redirect: string = `https://${import.meta.env.VITE_CLIENT_URL}${import.meta.env.VITE_REDIRECT_URI}`
+    const params = new URLSearchParams({
             response_type: import.meta.env.VITE_RESPONSE_TYPE,
             client_id: import.meta.env.VITE_SPOTIFY_CLIENT_ID,
             scope: import.meta.env.VITE_SCOPES,
-            redirect_uri: import.meta.env.VITE_REDIRECT_URI
+            redirect_uri: redirect
         });
 
         return window.location.href = `https://accounts.spotify.com/authorize?${params.toString()}`
